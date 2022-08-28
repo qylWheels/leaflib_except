22db91de38800dce3b7105aaedc0c68cfdde037f
qylWheels <command1748165360@126.com>
2022-08-28 17:32:58 +0800

    Implementations of all functions and macros.

--- leaf_except.h
+++ leaf_except.h
@@ -8,6 +8,9 @@
 #ifndef __LEAF_EXCEPT_H__
 #define __LEAF_EXCEPT_H__
 
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <setjmp.h>
 
 #ifdef __cplusplus
@@ -22,35 +25,96 @@ _LEAF_BEGIN_DECLS
 
 /* Data structures */
 typedef const char              *LeafExceptInfo;
-typedef struct _LeafExceptFrame *LeafExceptFrame;
+typedef struct _LeafExceptFrame _LeafExceptFrame;
 struct _LeafExceptFrame {
-    LeafExceptFrame *prev;
-    jmp_buf          env;
-    const char      *file;
-    int              line;
-    LeafExceptInfo  *e;
+    _LeafExceptFrame *prev;
+    jmp_buf           env;
+    const char       *file;
+    int               line;
+    LeafExceptInfo   *expection;
+};
+_LeafExceptFrame *_leaf_except_stack = NULL;
+
+
+/* Internal Status */
+enum {
+    _kLeafExceptEntered = 0,
+    _kLeafExceptRaised,
+    _kLeafExceptHandled,
+    _kLeafExceptFinalized
 };
 
 /* Internal function */
 static inline void _raise_exception(LeafExceptInfo *e, const char *file,
                                     int line) {
-
+    assert(e);
+    _LeafExceptFrame *p = _leaf_except_stack;
+    if (p == NULL) {
+        fprintf(stderr, "Uncaught exception");
+        if (*e)
+            fprintf(stderr, " %s", *e);
+        else
+            fprintf(stderr, " 0x%p", e);
+        if (file && line > 0)
+            fprintf(stderr, " raised at [%s: %d]\n", file, line);
+        fprintf(stderr, "aborting...\n");
+        fflush(stderr);
+        abort();
+    }
+    p->expection = e;
+    p->file = file;
+    p->line = line;
+    _leaf_except_stack = _leaf_except_stack->prev;
+    longjmp(p->env, _kLeafExceptRaised);
+    return;
 }
 
 /* Exported macros */
-#define TRY
+#undef TRY
+#define TRY                                                 \
+do {                                                        \
+    volatile int _leaf_except_flag;                         \
+    _LeafExceptFrame _leaf_except_frame;                    \
+    _leaf_except_frame.prev = _leaf_except_stack;           \
+    _leaf_except_stack = &_leaf_except_frame;               \
+    _leaf_except_flag = setjmp(_leaf_except_frame.env);     \
+    if (_leaf_except_flag == _kLeafExceptEntered) {
 
-#define EXCEPT(e)
+#define EXCEPT(e)                                           \
+        if (_leaf_except_flag == _kLeafExceptEntered)       \
+            _leaf_except_stack = _leaf_except_stack->prev;  \
+    } else if (_leaf_except_frame.expection == &(e)) {      \
+        _leaf_except_flag = _kLeafExceptHandled;
 
-#define ELSE
+#define ELSE                                                \
+        if (_leaf_except_flag == _kLeafExceptEntered)       \
+            _leaf_except_stack = _leaf_except_stack->prev;  \
+    } else {                                                \
+        _leaf_except_flag = _kLeafExceptHandled;
 
-#define FINALLY
+#define FINALLY                                             \
+        if (_leaf_except_flag == _kLeafExceptEntered)       \
+            _leaf_except_stack = _leaf_except_stack->prev;  \
+    } {                                                     \
+        if (_leaf_except_flag == _kLeafExceptEntered)       \
+            _leaf_except_flag = _kLeafExceptFinalized;
 
-#define END_TRY
+#define END_TRY                                             \
+        if (_leaf_except_flag == _kLeafExceptEntered)       \
+            _leaf_except_stack = _leaf_except_stack->prev;  \
+    }                                                       \
+    if (_leaf_except_flag == _kLeafExceptRaised)            \
+        _raise_exception(_leaf_except_frame.expection,      \
+                         __FILE__,                          \
+                         __LINE__);                         \
+} while (0);
 
-#define RAISE(e)
+#define RAISE(e) _raise_exception(&(e), __FILE__, __LINE__);
+
+#define RETURN                                              \
+    _leaf_except_stack = _leaf_except_stack->prev;          \
+    return
 
 _LEAF_END_DECLS
 
 #endif  /* __LEAF_EXCEPT_H__ */
-
